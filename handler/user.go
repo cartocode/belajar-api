c9c7e2664a6a110463f1aef36afeab6388f72a33
cartocode <carto6670@gmail.com>
2022-03-14 14:01:15 +0700

    login enpoint

--- handler/user.go
+++ handler/user.go
@@ -46,3 +46,39 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 	// c.JSON(http.StatusOK, nil)
 	c.JSON(http.StatusOK, jsonResponse)
 }
+
+func (h *userHandler) Login(c *gin.Context) {
+	// user memasukan input
+	// input ditangkap handler
+	// maping dari input user ke struct
+	// input struct passing service
+	// diservice mencari dengan bantuan repository user dengan email x
+	// mencocokan password
+	var input user.LoginInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Login Failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	loggedinUser,err := h.userService.Login(input)
+
+	if err != nil {
+		errorMessage := gin.H{"errors":err.Error()}
+
+		response := helper.APIResponse("Login Failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	formatter := user.FormatUser(loggedinUser, "tokentokentoken")
+
+	response := helper.APIResponse("Successfuly loggedin", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+}
--- main.go
+++ main.go
@@ -21,6 +21,28 @@ func main() {
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
 
+	// userByEmail, err := userRepository.FindByEmail("jsonResponse@gmail.com")
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+	// fmt.Println(userByEmail.Name)
+	// if (userByEmail.ID == 0) {
+	// 	fmt.Println("User tidak ditemukan")
+	// } else {
+	// 	fmt.Println(userByEmail.Name)
+	// }
+	// input := user.LoginInput {
+	// 	Email: "jsonResponse@gmail.com",
+	// 	Password: "password",
+	// }
+	// user, err := userService.Login(input)
+	// if err != nil {
+	// 	fmt.Println("Terjadi kesalahan")
+	// 	fmt.Println(err.Error())
+	// }
+	// fmt.Println(user.Email)
+	// fmt.Println(user.Name)
+
 	userHandler := handler.NewUserHandler(userService)
 
 	router := gin.Default()
@@ -28,6 +50,8 @@ func main() {
 	api := router.Group("/api/v1")
 
 	api.POST("/users", userHandler.RegisterUser)
+	api.POST("/sessions", userHandler.Login)
+	
 	router.Run()
 
 	// input dari user
