8e1999f20569d6388f4508f0893ba7a750383845
cartocode <carto6670@gmail.com>
2022-03-19 20:47:01 +0700

    create type campaign and campaigImage untuk file enity

--- campaign/entity.go
+++ campaign/entity.go
@@ -1,1 +1,26 @@
 package campaign
+
+import "time"
+
+type Campaign struct {
+	ID               int
+	UserID           int
+	Name             string
+	ShortDescription string
+	Description      string
+	Parks            string
+	BackerCount      int
+	GoalAmount       int
+	CurrentAmmount   int
+	Slug             string
+	CreatedAt        time.Time
+	UpdatedAt 			 time.Time
+}
+type CampaignImage struct {
+	ID int
+	CampaignID int
+	FileName string
+	IsPrimary int
+	CreatedAt        time.Time
+	UpdatedAt 			 time.Time
+}
